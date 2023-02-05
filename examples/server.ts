b24f39340f94bc722861f733841b0deb928d63c6
Atom <andrea.diblasix@gmail.com>
2023-02-05 13:39:57 +0100

    0.1.0

---
+++ examples/server.ts
@@ -0,0 +1,61 @@
+import Fastify from 'fastify'
+import fastifySharedSchema from '../dist'
+
+async function loader(){
+  const fastify = Fastify({
+    logger: true
+  })
+  await fastify.register(fastifySharedSchema, {
+    commonSchema: {
+      body: {
+        type: 'object',
+        properties: {
+          name: {type: 'string'}
+        },
+        required: ["name"]
+      }
+    },
+    routesToApply: {
+      "product": ['post', 'get', 'patch']
+    }
+  });
+
+  fastify.post("/test", {
+    handler: async (req,res) => {
+      return res.send({hello:"world"})
+    },
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            value: { type: 'string' },
+            otherValue: { type: 'boolean' }
+          }
+        }
+      },
+      body: {
+        type: 'object',
+        properties: {
+          lastname: { type: 'string'}
+        },
+      },
+    }})
+
+  fastify.patch("/test", {
+    handler: async (req,res) => {
+      return res.send({hello:"world"})
+    },
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          email: { type: 'string'}
+        },
+      },
+    }})
+
+  fastify.listen(3000)
+}
+
+loader()
